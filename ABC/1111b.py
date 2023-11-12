0a303df07d8e3bd42161d3f0f7fca6b55e3fbd56
y177649 <22311092ys@tama.ac.jp>
2023-11-12 20:57:03 +0900

    1111bX

--- ABC/1111b.py
+++ ABC/1111b.py
@@ -1,11 +1,10 @@
-n,x=input().split()
-s=list(map(int,input().split()))
-s.sort()
+n=int(input())
+d=list(map(int,input().split()))
 cou=0
-for f1 in range(n):
-    if x>=s[f1]:
-        cou+=s[f1]
-    else:
-        break
+for f1 in range(len(d)):
+    if d[f1]==f1+1:
+        cou+=1
+    elif d[f1]>=f1*11:
+        cou+=f1+1
 
 print(cou)
