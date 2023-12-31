90b91d67607ea79a1681ece7cc45d1a389c385ff
y177649 <22311092ys@tama.ac.jp>
2023-12-31 14:55:29 +0900

    add a65

--- q_past/a65.py
+++ q_past/a65.py
@@ -1,1 +1,19 @@
 # This is file 65
+"""
+a-b if x =2 <1 >3
+
+a-b if + 3
+
+"""
+pri='null'
+x,a,b=list(map(int,input().split()))
+
+if a-b>1:
+    pri='delicious'
+else:
+    if -x<(a-b):
+        pri='safe'
+    else:
+        pri='dangerous'
+
+print(pri)
