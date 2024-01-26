1cb12b566fb57ebca802ccaa68c0cd321fb0d901
[y177649] <[22311092ys@tama.ac.jp]>
2024-01-27 07:25:27 +0900

    update b008.py

---
+++ past_b/b008.py
@@ -0,0 +1,60 @@
+x
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+n=int(input())
+
+s_list=[]
+for f1 in range(n):
+    s=input()
+    s_list.append(s)
+
+s_set_list=list(set(s_list))
+
+#print(s_set_list)
+cou=[]
+for f2 in range(len(s_set_list)):
+    for f3 in range(len(s_list)):
+        if s_set_list[f2]==s_list[f3]:
+            cou.append('y')
+        else:
+            cou.append('n')
+
+for f4 in range(len(s_list)):
+"""
