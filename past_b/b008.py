5eb5a9af8d1fffae9596fbab7199f496fa69f581
[y177649] <[22311092ys@tama.ac.jp]>
2024-01-30 12:47:17 +0900

    add input 336

--- past_b/b008.py
+++ past_b/b008.py
@@ -1,4 +1,17 @@
-x
+
+#input_________
+
+II=lambda:int(input())
+MIS=lambda:map(int,input().split())
+LMI=lambda:list(map(int,input()))
+LMIS=lambda:list(map(int,input().split()))
+I=lambda:input()
+L=lambda:list(input())
+S=lambda:input().split()
+FI=[input() for _ in range(n)]
+
+#code___________
+
 
 
 
