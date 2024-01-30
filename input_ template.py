ff1ab0a725544c2829b9617cc0c9a78ecb40acde
[y177649] <[22311092ys@tama.ac.jp]>
2024-01-30 12:50:33 +0900

    add input origin

--- input_ template.py
+++ input_ template.py
@@ -7,6 +7,6 @@ LMIS=lambda:list(map(int,input().split()))
 I=lambda:input()
 L=lambda:list(input())
 S=lambda:input().split()
-#FI=[input() for _ in range(n)]
+FI=[input() for _ in range(n)]
 
 #code___________
