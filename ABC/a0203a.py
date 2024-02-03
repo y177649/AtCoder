dd2ca147be9ab9eaf738e280230fc85df7d8f81f
[y177649] <[22311092ys@tama.ac.jp]>
2024-02-03 22:18:57 +0900

    add abc0203

---
+++ ABC/a0203a.py
@@ -0,0 +1,15 @@
+#input_________
+
+II=lambda:int(input())
+MIS=lambda:map(int,input().split())
+LMI=lambda:list(map(int,input()))
+LMIS=lambda:list(map(int,input().split()))
+I=lambda:input()
+L=lambda:list(input())
+S=lambda:input().split()
+#FI=[input() for _ in range(n)]
+
+#code___________
+
+x=input().split('.')
+print(x[-1])
