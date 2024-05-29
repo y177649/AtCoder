3ea3cdb7813a04f486ba2eebe6eaa41ec4df81a5
sato <22311092ys@tama.ac.jp>
2024-05-29 21:17:46 +0900

    add b010.py

--- past_b/b010.py
+++ past_b/b010.py
@@ -8,7 +8,21 @@ LMIS=lambda:list(map(int,input().split()))
 I=lambda:input()
 L=lambda:list(input())
 S=lambda:input().split()
-FI=[input() for _ in range(n)]
+#FI=[input() for _ in range(n)]
 
 #code___________
 
+n=II()
+a=LMIS()
+
+ans=0
+for i in range(n):
+    if a[i] % 2 == 0:
+        a[i]-=1
+        ans+=1
+
+for j in a:
+    if j % 3 == 2:
+        ans+=2
+
+print(ans)
