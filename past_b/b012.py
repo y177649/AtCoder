22ebb9972f9f30190e5ac4fd7b295ae9969813a4
sato <22311092ys@tama.ac.jp>
2024-06-01 00:49:19 +0900

    add b012.py

--- past_b/b012.py
+++ past_b/b012.py
@@ -8,7 +8,16 @@ LMIS=lambda:list(map(int,input().split()))
 I=lambda:input()
 L=lambda:list(input())
 S=lambda:input().split()
-FI=[input() for _ in range(n)]
+#FI=[input() for _ in range(n)]
 
 #code___________
 
+n = II()
+
+h = n // 3600
+a = n % 3600
+
+m = a // 60
+s = a % 60
+
+print(f"{h:02}"+':'+f"{m:02}"+':'+f"{s:02}")
