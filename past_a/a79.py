400884e3d7791cd326667b8090c0e74d22f42fa5
sato <22311092ys@tama.ac.jp>
2024-06-12 23:42:47 +0900

    add a79.py

--- past_a/a79.py
+++ past_a/a79.py
@@ -1,1 +1,11 @@
 # This is file 79
+
+n=input()
+
+if n[0] == n[1] and n[0] == n[2]:
+    print('Yes')
+else:
+    if n[1] == n[2] and n[1] == n[3]:
+        print('Yes')
+    else:
+        print('No')
