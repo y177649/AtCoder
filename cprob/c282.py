3d875848c5d23311f7243bd67ab655ca6c54c1df
sato <22311092ys@tama.ac.jp>
2024-06-13 22:27:43 +0900

    add cfile

---
+++ cprob/c282.py
@@ -0,0 +1,12 @@
+# ========== input ===========
+
+II=lambda:int(input())
+MIS=lambda:map(int,input().split())
+LMI=lambda:list(map(int,input()))
+LMIS=lambda:list(map(int,input().split()))
+I=lambda:input()
+L=lambda:list(input())
+S=lambda:input().split()
+#FI=[input() for _ in range(n)]
+
+# ========== source code ===========
