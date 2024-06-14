5aa73bd4085e6a444475c34d8b8f6ff07267557f
sato <22311092ys@tama.ac.jp>
2024-06-14 22:13:14 +0900

    add b015.py

--- bprob/b015.py
+++ bprob/b015.py
@@ -1,4 +1,3 @@
-
 #input_________
 
 II=lambda:int(input())
@@ -8,7 +7,24 @@ LMIS=lambda:list(map(int,input().split()))
 I=lambda:input()
 L=lambda:list(input())
 S=lambda:input().split()
-FI=[input() for _ in range(n)]
+#FI=[input() for _ in range(n)]
 
 #code___________
 
+n = II()
+a = LMIS()
+
+count = n
+for i in range(n) :
+    if a[i] == 0 :
+        count -= 1
+
+ans = sum(a)//count
+#dev = sum(a) % count
+
+if sum(a) % count != 0 :
+    ans += 1
+
+print(ans)
+#print(sum(a))
+#print(dev)
