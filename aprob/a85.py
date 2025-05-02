be5df2fed0e1ded5cd504de3af9704c973637b02
sato <22311092ys@tama.ac.jp>
2025-05-02 11:42:22 +0900

    add a85.py

--- aprob/a85.py
+++ aprob/a85.py
@@ -1,1 +1,14 @@
 # This is file 85
+
+yyyymmdd=list(input())
+
+yyyymmdd[3]='8'
+
+"""
+map_yyyymmdd = map(str,yyyymmdd)
+
+join_yyyymmdd = ''.join(map_yyyymmdd)
+"""
+join_map_yyyymmdd = ''.join(map(str,yyyymmdd))
+
+print(join_map_yyyymmdd)
