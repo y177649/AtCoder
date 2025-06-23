e46cc688bbbb2ccab74fec008d86be28dd9df218
sato <22311092ys@tama.ac.jp>
2025-06-23 23:49:34 +0900

    Fix: forでi/oを効率化

--- aprob/a92.py
+++ aprob/a92.py
@@ -1,5 +1,6 @@
 # This is file 92
 
+"""
 a = int(input())
 b = int(input())
 c = int(input())
@@ -19,3 +20,27 @@ else:
     result_cd = d
 
 print(result_ab + result_cd)
+"""
+
+"""
+ls = []
+x=0
+
+result_ab = 0
+result_cd = 0
+
+for i in range(4):
+    x = int(input())
+    ls.append(x)
+
+if ls[0] < ls[1]:
+    result_ab = ls[0]
+else:
+    result_ab = ls[1]
+if ls[2] < ls[3]:
+    result_cd = ls[2]
+else:
+    result_cd = ls [3]
+
+print(result_ab + result_cd)
+"""
