7f449777efa1a5bec229b9662439f6ce4072ec5b
sato <22311092ys@tama.ac.jp>
2025-06-23 23:28:51 +0900

    Add: a92.py

--- aprob/a92.py
+++ aprob/a92.py
@@ -1,1 +1,21 @@
 # This is file 92
+
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+result_ab = 0
+result_cd = 0
+
+if a<b :
+    result_ab = a
+else:
+    result_ab = b
+
+if c<d :
+    result_cd = c
+else:
+    result_cd = d
+
+print(result_ab + result_cd)
